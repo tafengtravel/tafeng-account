6eaa636fef7ffccdf1f21a7e7842afc75d223e7f
kevin860320 <kevin860320@gmail.com>
2021-11-28 16:42:49 +0800

    Update Input.vue

--- src/components/Input.vue
+++ src/components/Input.vue
@@ -91,17 +91,17 @@
           <div class="font">團費</div>
         </el-row>
         <span v-for="(priceDetail,index) in ruleForm.priceDetail">
-          <el-form-item :label="'品項'+(index+1).toString()" prop="item" label-width="60px">
+          <el-form-item :label="'品項'+(index+1).toString()" prop="priceDetail.item" label-width="50px">
             <el-input v-model="priceDetail.item" style="width: 250px;" @input="count" ></el-input>
           </el-form-item>            
-          <el-form-item label="單價" prop="itemPrice">
-            <el-input v-model="priceDetail.itemPrice" style="width: 100px;" @input="count"></el-input>
+          <el-form-item label="單價" prop="priceDetail.price">
+            <el-input v-model="priceDetail.price" style="width: 100px;" @input="count"></el-input>
           </el-form-item>
-          <el-form-item label="x 人數" prop="itemAmount">
-            <el-input v-model="priceDetail.itemAmount" style="width: 100px;" @input="count"></el-input>
+          <el-form-item label="x 人數" prop="priceDetail.amount">
+            <el-input v-model="priceDetail.amount" style="width: 100px;" @input="count"></el-input>
           </el-form-item>
-          <el-form-item label="= 費用" prop="itemTotalPrice" @input="count">
-            <el-input v-model="priceDetail.itemTotalPrice" disabled ></el-input>
+          <el-form-item label="= 費用" prop="priceDetail.totalPrice" @input="count">
+            <el-input v-model="priceDetail.totalPrice" disabled ></el-input>
           </el-form-item>
           <el-row></el-row>
         </span>
@@ -114,25 +114,89 @@
           <div class="font">加購項目</div>
         </el-row>
         <span v-for="(priceExtraDetail,index) in ruleForm.priceExtraDetail">
-          <el-form-item :label="'品項'+(index+1).toString()" prop="item" label-width="60px">
+          <el-form-item :label="'品項'+(index+1).toString()" prop="priceExtraDetail.item" label-width="50px">
             <el-input v-model="priceExtraDetail.item" style="width: 250px;" @input="count" ></el-input>
           </el-form-item>            
-          <el-form-item label="單價" prop="itemPrice">
-            <el-input v-model="priceExtraDetail.itemPrice" style="width: 100px;" @input="count"></el-input>
+          <el-form-item label="單價" prop="priceExtraDetail.price">
+            <el-input v-model="priceExtraDetail.price" style="width: 100px;" @input="count"></el-input>
           </el-form-item>
-          <el-form-item label="x 數量" prop="itemAmount">
-            <el-input v-model="priceExtraDetail.itemAmount" style="width: 100px;" @input="count"></el-input>
+          <el-form-item label="x 數量" prop="priceExtraDetail.amount">
+            <el-input v-model="priceExtraDetail.amount" style="width: 100px;" @input="count"></el-input>
           </el-form-item>
-          <el-form-item label="x 天數" prop="itemDays">
-            <el-input v-model="priceExtraDetail.itemDays" style="width: 100px;" @input="count"></el-input>
+          <el-form-item label="x 天數" prop="priceExtraDetail.days">
+            <el-input v-model="priceExtraDetail.days" style="width: 100px;" @input="count"></el-input>
           </el-form-item>
-          <el-form-item label="= 費用" prop="itemTotalPrice">
-            <el-input v-model="priceExtraDetail.itemTotalPrice" disabled ></el-input>
+          <el-form-item label="= 費用" prop="priceExtraDetail.totalPrice">
+            <el-input v-model="priceExtraDetail.totalPrice" disabled ></el-input>
           </el-form-item>
           <el-row></el-row>
         </span>
         <el-button type="primary" icon="el-icon-circle-plus-outline" @click="priceExtraDetailAdd();count()"></el-button>
         <el-button type="danger" icon="el-icon-remove-outline" @click="priceExtraDetailRemove();count()"></el-button>
+
+        <el-row></el-row>
+
+        <el-row :gutter="20">
+          <div class="font">收入</div>
+        </el-row>
+        <span v-for="(incomeDetail,index) in ruleForm.incomeDetail">
+          <el-row :gutter="20">
+            <div class="font">{{index+1}}.</div>
+          </el-row>
+          
+          <el-form-item label="報帳日期" prop="incomeDetail.date">
+            <el-date-picker type="date" value-format="yyyy-MM-dd" placeholder="選擇日期" v-model="incomeDetail.date" style="width: 190px;"></el-date-picker>
+          </el-form-item>
+          <el-form-item label="品項" prop="incomeDetail.item">
+            <el-input v-model="incomeDetail.item"></el-input>
+          </el-form-item> 
+          <el-form-item label="收入" prop="incomeDetail.income">
+            <el-input v-model="incomeDetail.income" @input="count" ></el-input>
+          </el-form-item> 
+          <el-form-item label="收款方式" prop="incomeDetail.type" >
+            <el-select v-model="incomeDetail.type" placeholder="收款方式" style="width: 110px;">
+              <el-option label="匯款" value="匯款"></el-option>
+              <el-option label="刷卡" value="刷卡"></el-option>
+              <el-option label="現金" value="現金"></el-option>
+              <el-option label="支票" value="支票"></el-option>
+              <el-option label="其他" value="其他"></el-option>
+              <el-option label="五倍券" value="五倍券"></el-option>
+              <el-option label="LINEPAY" value="LINEPAY"></el-option>
+            </el-select>
+          </el-form-item>
+          <el-form-item label="國旅卡" prop="incomeDetail.card" >
+            <el-select v-model="incomeDetail.card" placeholder="國旅卡" style="width: 110px;">
+              <el-option label="否" value="false"></el-option>
+              <el-option label="是" value="true"></el-option>
+            </el-select>
+          </el-form-item>
+          <el-form-item label="收款日期" prop="incomeDetail.receiveDate">
+            <el-date-picker type="date" value-format="yyyy-MM-dd" placeholder="選擇日期" v-model="incomeDetail.receiveDate" style="width: 190px;"></el-date-picker>
+          </el-form-item>
+          <el-form-item label="收款證明" prop="incomeDetail.prove">
+            <el-input v-model="incomeDetail.prov" ></el-input>
+          </el-form-item> 
+          <el-form-item label="備註" prop="incomeDetail.other">
+            <el-input v-model="incomeDetail.other" ></el-input>
+          </el-form-item> 
+          <el-form-item label="OP核實" prop="incomeDetail.opCheck" >
+            <el-select v-model="incomeDetail.opCheck" placeholder="OP核實" style="width: 110px;">
+              <el-option label="否" value="false"></el-option>
+              <el-option label="是" value="true"></el-option>
+            </el-select>
+          </el-form-item>
+          <el-form-item label="財務核實" prop="incomeDetail.adminCheck" >
+            <el-select v-model="incomeDetail.adminCheck" placeholder="財務核實" style="width: 110px;">
+              <el-option label="否" value="false"></el-option>
+              <el-option label="是" value="true"></el-option>
+            </el-select>
+          </el-form-item>
+          <el-row></el-row>
+        </span>
+        
+
+        <el-button type="primary" icon="el-icon-circle-plus-outline" @click="incomeDetailAdd();count()"></el-button>
+        <el-button type="danger" icon="el-icon-remove-outline" @click="incomeDetailRemove();count()"></el-button>
 
         <el-row></el-row>
 
@@ -345,6 +409,7 @@ export default {
         cancel:'false',
         priceDetail:[],
         priceExtraDetail:[],
+        incomeDetail:[],
       },
       rules: {
           number: [
@@ -384,6 +449,12 @@ export default {
      
   },
   methods: {
+    incomeDetailAdd(){
+      this.ruleForm.incomeDetail.push({opCheck:'false',adminCheck:'false',card:'false',type:'匯款'});
+    },
+    incomeDetailRemove(){
+      this.ruleForm.incomeDetail.pop({});
+    },
     priceDetailAdd(){
       this.ruleForm.priceDetail.push({});
     },
@@ -406,12 +477,12 @@ export default {
     count(){
       this.ruleForm.totalPrice = 0
       for(let i=0;i<this.ruleForm.priceDetail.length;i++){
-        this.ruleForm.priceDetail[i].itemTotalPrice = parseInt(this.ruleForm.priceDetail[i].itemAmount) * parseInt(this.ruleForm.priceDetail[i].itemPrice)
-        this.ruleForm.totalPrice = parseInt(this.ruleForm.totalPrice) + parseInt(this.ruleForm.priceDetail[i].itemTotalPrice)
+        this.ruleForm.priceDetail[i].totalPrice = parseInt(this.ruleForm.priceDetail[i].amount) * parseInt(this.ruleForm.priceDetail[i].price)
+        this.ruleForm.totalPrice = parseInt(this.ruleForm.totalPrice) + parseInt(this.ruleForm.priceDetail[i].totalPrice)
       }
       for(let i=0;i<this.ruleForm.priceExtraDetail.length;i++){
-        this.ruleForm.priceExtraDetail[i].itemTotalPrice = parseInt(this.ruleForm.priceExtraDetail[i].itemAmount) * parseInt(this.ruleForm.priceExtraDetail[i].itemPrice) * parseInt(this.ruleForm.priceExtraDetail[i].itemDays)
-        this.ruleForm.totalPrice = parseInt(this.ruleForm.totalPrice) + parseInt(this.ruleForm.priceExtraDetail[i].itemTotalPrice)
+        this.ruleForm.priceExtraDetail[i].totalPrice = parseInt(this.ruleForm.priceExtraDetail[i].amount) * parseInt(this.ruleForm.priceExtraDetail[i].price) * parseInt(this.ruleForm.priceExtraDetail[i].days)
+        this.ruleForm.totalPrice = parseInt(this.ruleForm.totalPrice) + parseInt(this.ruleForm.priceExtraDetail[i].totalPrice)
       }
       
     },
