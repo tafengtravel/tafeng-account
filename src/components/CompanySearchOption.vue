07a118d73343816e4253bfbea9a4a8126471a310
tafengtravel <tafengtravel@gmail.com>
2022-05-26 13:46:51 +0800

    Update CompanySearchOption.vue

--- src/components/CompanySearchOption.vue
+++ src/components/CompanySearchOption.vue
@@ -43,59 +43,6 @@ export default {
     return {
       month:'',
       date:[],
-      csOptions: [{
-        value: 'all',
-        label: '全部'
-      },{
-        value: 'A1',
-        label: 'A1'
-      },{
-        value: 'A2',
-        label: 'A2'
-      },{
-        value: 'A3',
-        label: 'A3'
-      },{
-        value: 'A4',
-        label: 'A4'
-      },{
-        value: 'B1',
-        label: 'B1'
-      },{
-        value: 'B2',
-        label: 'B2'
-      },{
-        value: 'B3',
-        label: 'B3'
-      },{
-        value: 'B4',
-        label: 'B4'
-      },{
-        value: 'B5',
-        label: 'B5'
-      },{
-        value: 'B6',
-        label: 'B6'
-      },{
-        value: 'B7',
-        label: 'B7'
-      },{
-        value: 'B8',
-        label: 'B8'
-      },{
-        value: 'B9',
-        label: 'B9'
-      },{
-        value: 'B10',
-        label: 'B10'
-      },{
-        value: 'B11',
-        label: 'B11'
-      },{
-        value: 'B12',
-        label: 'B12'
-      }],
-      cs: 'all',
       company:''
     }
   },
