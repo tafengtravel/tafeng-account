74a19e5e3045ca1bbaa50fafd65bf9a759840cf0
tafengtravel <tafengtravel@gmail.com>
2022-07-08 17:11:50 +0800

    update

--- src/views/ticket/mandarin-phone/index.vue
+++ src/views/ticket/mandarin-phone/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="dashboard-container">
-    
+      <Ticket @passengerChildEvent="passengerToConsole" ref="child"></Ticket>
   </div>
 </template>
 
@@ -8,23 +8,48 @@
 import axios from 'axios'
 import * as moment from "moment/moment";
 import '@/styles/common.css'
+import Ticket from '@/components/Ticket.vue'
 
 export default {
-  name: 'Dashboard',
+  components: { 
+    Ticket 
+  },
   data() {
     return {
-     
-    }
+
+    };
   },
   computed: {
-    
+
   },
   methods: {
-    
-  },
-  async mounted(){
+    passengerToConsole(passengerData){
+      console.log(passengerData)
+      let consoleStr = ''
+      let lastName = 'lastName_'
+      let firstName = 'firstName_'
+      let birth = 'birthyDate_'
+      let id = 'Idnum_'
+      let value = '.value ='
+      let quote = '"'
+      
 
-  }
+      passengerData.forEach((data, index, object) => {
+        consoleStr = consoleStr +
+        lastName + index.toString() + value + quote + data.name.substring(0,1) + quote + '\n' +
+        firstName + index.toString() + value + quote + data.name.substring(1,data.name.length) + quote + '\n' +
+        birth + index.toString() + value + quote + data.birth + quote + '\n' +
+        id + index.toString() + value + quote + data.id + quote + '\n' 
+      });
+      
+      consoleStr = consoleStr + 'contname.value = "呂孟庭" \ncontpsgname.value = "呂孟庭" \nNightphone_Code.value = "0933341445"' 
+      console.log(consoleStr)
+      this.$refs.child.passengerConsole = consoleStr
+    }
+  },
+  async mounted() {
+
+  },
 }
 </script>
 
@@ -38,27 +63,5 @@ export default {
     line-height: 46px;
   }
 }
-.sub_title{
-  color: black;
-  // font-family: Microsoft YaHei;
-  // font-weight: 500;
-}
-.app-container {
-  padding: 7px;
-}
-.form-font-md{
-  font-family: 'Microsoft JhengHei';
-}
 </style>
-<style>
-@media (min-width: 768px){
-  .el-message-box {
-    width: 20%;
-  }
-}
-@media (max-width: 768px) {
-  .el-message-box {
-    width: 90%;
-  }
-}
-</style>
+
--- src/views/ticket/uni/index.vue
+++ src/views/ticket/uni/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="dashboard-container">
-    
+      <Ticket @passengerChildEvent="passengerToConsole" ref="child"></Ticket>
   </div>
 </template>
 
@@ -8,23 +8,66 @@
 import axios from 'axios'
 import * as moment from "moment/moment";
 import '@/styles/common.css'
+import Ticket from '@/components/Ticket.vue'
 
 export default {
-  name: 'Dashboard',
+  components: { 
+    Ticket 
+  },
   data() {
     return {
-     
-    }
+
+    };
   },
   computed: {
-    
+
   },
   methods: {
-    
-  },
-  async mounted(){
+    passengerToConsole(passengerData){
+      console.log(passengerData)
 
-  }
+      let consoleStr = ''
+      let value = '.value ='
+      let quote = '"'
+
+      let roundTicket = '"YBC+YEBC"'
+      let singleTicket = '"YEBC"'
+      let email = 'CPH_Body_tb_Email.value = "tafengtravel@gmail.com"'
+      let ticket = 'CPH_Body_rpt_PassengerList_ddl_Farebasis_'
+      let id = 'CPH_Body_rpt_PassengerList_tb_ID_NO_'
+      let birth1 = 'CPH_Body_rpt_PassengerList_tb_Birthday_'
+      let birth2 = 'CPH_Body_rpt_PassengerList_hi_Birthday_'
+      let country = 'CPH_Body_rpt_PassengerList_hi_Country_'
+      let countryValue = 'TW'
+      let gender = 'CPH_Body_rpt_PassengerList_ddl_Title_'
+      let lastName = 'CPH_Body_rpt_PassengerList_tb_LastName_'
+      let firstName = 'CPH_Body_rpt_PassengerList_tb_FirstName_'
+
+      passengerData.forEach((data, index, object) => {
+        let genderValue
+        if(data.id.substring(1,2) == '1'){
+          genderValue = 'MR'
+        }else if (data.id.substring(1,2) == '2'){
+          genderValue = 'MS'
+        }
+
+        consoleStr = consoleStr +
+        country + index.toString() + value + quote + countryValue + quote + '\n' +
+        lastName + index.toString() + value + quote + data.name.substring(0,1) + quote + '\n' +
+        firstName + index.toString() + value + quote + data.name.substring(1,data.name.length) + quote + '\n' +
+        birth1 + index.toString() + value + quote + moment(data.birth).format('YYYY/MM/DD') + quote + '\n' +
+        birth2 + index.toString() + value + quote + moment(data.birth).format('YYYY/MM/DD') + quote + '\n' +
+        gender + index.toString() + value + quote + genderValue + quote + '\n' +
+        id + index.toString() + value + quote + data.id + quote + '\n' 
+      });
+     
+      console.log(consoleStr)
+      this.$refs.child.passengerConsole = consoleStr
+    }
+  },
+  async mounted() {
+
+  },
 }
 </script>
 
@@ -38,27 +81,5 @@ export default {
     line-height: 46px;
   }
 }
-.sub_title{
-  color: black;
-  // font-family: Microsoft YaHei;
-  // font-weight: 500;
-}
-.app-container {
-  padding: 7px;
-}
-.form-font-md{
-  font-family: 'Microsoft JhengHei';
-}
 </style>
-<style>
-@media (min-width: 768px){
-  .el-message-box {
-    width: 20%;
-  }
-}
-@media (max-width: 768px) {
-  .el-message-box {
-    width: 90%;
-  }
-}
-</style>
+
